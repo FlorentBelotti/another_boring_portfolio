281202350cd784d61b1a4b6e3e6669554ea9d660
Florent Belotti <florent.l.d.belotti@gmail.com>
2025-11-06 21:35:17 +0100

    logo ok with fixed

---
+++ another_boring_portfolio/src/components/fragments/home/logo.tsx
@@ -0,0 +1,19 @@
+import styles from './logo.module.scss'
+import nero from '../../../assets/images/img_nero.png'
+
+export default function Logo() {
+  return (
+    <div className={styles.logo_container}>
+        <div className={styles.img_container}>
+            <img src={nero} alt="Nero" className={styles.logo_image} />
+        </div>
+        <div className={styles.logo_title}>
+            <div className={styles.logo_title_1}>
+                <div className={styles.text_1}> ANOTHER<br></br>BORING </div>
+                <div className={styles.eye}> </div>
+            </div>
+            <div className={styles.text_2}> PORTFOLIO </div>
+        </div>
+    </div>
+  )
+}
--- another_boring_portfolio/src/components/pages/home.tsx
+++ another_boring_portfolio/src/components/pages/home.tsx
@@ -1,16 +1,19 @@
 import styles from './home.module.scss'
+import building from '../../assets/images/img_building.png'
+import Logo from '../fragments/home/logo'
 
 export default function Home() {
   return (
     <>
       <section className={styles.block}>
-        <h2>Section 1</h2>
-        <p>Welcome to Home page section 1</p>
+        <Logo />
+
+        {/* <h2>Section 1</h2>
+        <p>Welcome to Home page section 1</p> */}
       </section>
       
       <section className={styles.block}>
-        <h2>Section 2</h2>
-        <p>Welcome to Home page section 2</p>
+        <img src={building} alt="Placeholder" className={styles.image} />
       </section>
       
       <section className={styles.block}>
