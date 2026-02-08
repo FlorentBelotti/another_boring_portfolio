842112772d47acc846d398d554c4cdbde76984aa
FlorentBelotti <florent.l.d.belotti@gmail.com>
2026-02-08 22:15:53 +0100

    pages ok

--- src/components/common/content.tsx
+++ src/components/common/content.tsx
@@ -1,31 +1,29 @@
-import { type ReactNode } from 'react'
-import styles from './Content.module.scss'
-import PageTransitionOverlay from './pageTransitionOverlay'
+import { type ReactNode } from "react";
+import styles from "./content.module.scss";
+import PageTransitionOverlay from "./pageTransitionOverlay";
 
 interface ContentProps {
-  leftBlock: ReactNode
-  centerBlock: ReactNode
-  rightBlock: ReactNode
+  leftBlock: ReactNode;
+  centerBlock: ReactNode;
+  rightBlock: ReactNode;
 }
 
-export default function Content({ leftBlock, centerBlock, rightBlock }: ContentProps) {
+export default function Content({
+  leftBlock,
+  centerBlock,
+  rightBlock,
+}: ContentProps) {
   return (
     <main className={styles.content}>
       <div className={styles.leftBlock}>
-        <PageTransitionOverlay>
-          {leftBlock}
-        </PageTransitionOverlay>
+        <PageTransitionOverlay>{leftBlock}</PageTransitionOverlay>
       </div>
       <div className={styles.centerBlock}>
-        <PageTransitionOverlay>
-          {centerBlock}
-        </PageTransitionOverlay>
+        <PageTransitionOverlay>{centerBlock}</PageTransitionOverlay>
       </div>
       <div className={styles.rightBlock}>
-        <PageTransitionOverlay>
-          {rightBlock}
-        </PageTransitionOverlay>
+        <PageTransitionOverlay>{rightBlock}</PageTransitionOverlay>
       </div>
     </main>
-  )
+  );
 }
--- src/components/fragments/common/separator.tsx
+++ src/components/fragments/common/separator.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styles from './Separator.module.scss';
+import React from "react";
+import styles from "./separator.module.scss";
 
 interface SeparatorProps {
   className?: string;
@@ -7,7 +7,7 @@ interface SeparatorProps {
 
 const Separator: React.FC<SeparatorProps> = ({ className }) => {
   return (
-    <div className={`${styles.separator} ${className || ''}`}>
+    <div className={`${styles.separator} ${className || ""}`}>
       <div className={styles.diamond}></div>
       <div className={styles.line}></div>
       <div className={styles.diamond}></div>
